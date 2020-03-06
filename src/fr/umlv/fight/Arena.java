793be8561b3e69de855371d4ad64c0d27d661c37
bmangeat <brice.mangeat@gmail.com>
2020-03-06 10:33:08 +0100

    add comments to exercise 1

--- src/fr/umlv/fight/Arena.java
+++ src/fr/umlv/fight/Arena.java
@@ -2,6 +2,13 @@ package fr.umlv.fight;
 
 public class Arena {
 
+
+    /**
+     * Fight between two robots
+     * @param r1 First robot which starts to fire
+     * @param r2 Second robot which can repost after
+     */
+
     public void fight(Robot r1, Robot r2){
         while (!r1.isDead() && !r2.isDead()){
             r1.fire(r2);
--- src/fr/umlv/fight/Main.java
+++ src/fr/umlv/fight/Main.java
@@ -2,13 +2,12 @@ package fr.umlv.fight;
 
 public class Main {
     public static void main(String[] args) {
+        // Create objects
         var d2r2 = new Robot("D2R2");
         var data = new Robot("Data");
         var arena = new Arena();
-        arena.fight(d2r2, data);
-        System.out.println(d2r2.get_lifePoint());
-        System.out.println(data.get_lifePoint());
 
+        arena.fight(d2r2, data);
 
     }
 }
